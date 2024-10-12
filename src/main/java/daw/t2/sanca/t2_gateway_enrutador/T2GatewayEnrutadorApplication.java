f96fba5e0ce2fe23698851630ecc4c96e13c2776
helzze <sebastiansancaowo@gmail.com>
2024-10-12 01:49:01 -0500

    gateway enrutador

---
+++ src/main/java/daw/t2/sanca/t2_gateway_enrutador/T2GatewayEnrutadorApplication.java
@@ -0,0 +1,13 @@
+package daw.t2.sanca.t2_gateway_enrutador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2GatewayEnrutadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2GatewayEnrutadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daw/t2/sanca/t2_gateway_enrutador/T2GatewayEnrutadorApplicationTests.java
@@ -0,0 +1,13 @@
+package daw.t2.sanca.t2_gateway_enrutador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2GatewayEnrutadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
